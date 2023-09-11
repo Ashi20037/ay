8a6eade300690d58cf2a62f4c715bc638998d18f
ashikakj2003@gmail.com <ashikakj2003@gmail.com>
2023-09-11 19:50:44 +0530

    com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./Pages/Home";
+import Books from "./Pages/Books";
+import Admin from "./Pages/Admin";
+import About from "./Pages/about";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Contact from "./Pages/Contact"
+import LibraryPolicies from "./Pages/Library_policies";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  return(
+   <>
+    <BrowserRouter>
+    {/* <Navbar/> */}
+    <Routes>
+      <Route path='/' element = {<Home/>} />
+      <Route path="/login" element={<Login/>}/>
+      <Route path='/books' element = {<Books/>} />
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path='/admin' element = {<Admin />} />
+      <Route path='/about' element = {<About />} />
+      <Route path='/contact' element = {<Contact />} />
+      <Route path='/libpolicy' element = {<LibraryPolicies />} />
+    </Routes>
+    </BrowserRouter>
+    </> 
+  )
 }
-
-export default App;
+export default App
